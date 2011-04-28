ed4ad77d02e108026a7c2e1c5a8cb2660112a1fb
Geegee <ggboyin@gmail.com>
2011-04-28 12:30:38 +0530

    search you pdf chm db

--- resources/scripts/python/chmpdfholder.py
+++ resources/scripts/python/chmpdfholder.py
@@ -1,5 +1,8 @@
 import sqlite3
 import os
+from Tkinter import *
+import tkMessageBox
+import Tkinter
 
 
 def lisfiles(path,files):
@@ -13,6 +16,29 @@ def lisfiles(path,files):
 
 
 
+def searchlist():
+    global listbox, entryWidget
+
+    if entryWidget.get().strip() == "":
+        tkMessageBox.showerror("Tkinter Entry Widget", "Enter a text value")
+    else:
+        listbox.delete(0,END)
+        populateList(entryWidget.get().strip(), listbox)
+
+
+def populateList(searchTerm, listbox):
+    db = sqlite3.connect("pdfchm.db")
+    cursor = db.cursor()
+    searchTerm = "%" + searchTerm + "%"
+    cursor.execute(""" SELECT name FROM pdfchms where name like :like """, {"like":searchTerm})    
+    for row in cursor:
+        listbox.insert(END, row)
+    db.close()
+     
+
+        
+    
+
 if __name__ == '__main__':
     db = sqlite3.connect("pdfchm.db")
     cursor = db.cursor()
@@ -24,25 +50,48 @@ if __name__ == '__main__':
 
     
     cursor.execute(""" CREATE TABLE IF NOT EXISTS pdfchms(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       name TEXT,size INTEGER, checksum TEXT ) """)
-    
-    
+                       name TEXT,size INTEGER, checksum TEXT ) """)    
     
     pdfs = []
     lisfiles("/home/geegee/Documents",pdfs)
 
+
     for row in pdfs:
-        print row[0]
         cursor.execute(" SELECT * FROM pdfchms where name = :name ",{"name":row[0]})
         r = cursor.fetchone()
         if not r:
             cursor.execute(""" INSERT INTO pdfchms (name) VALUES(?) """, tuple(row))
-            db.commit()
+            db.commit()         
          
-         
-    cursor.execute(""" SELECT * FROM pdfchms """)
+    cursor.execute(""" SELECT name FROM pdfchms """)
+
+    main = Tk()
+    main.title(" Collection of Pdf and chms")
+    main["padx"] = 10
+    main["pady"] = 10
+    textFrame = Frame(main)
+    
+    entryLabel = Label(textFrame)
+    entryLabel["text"] = "Search:"
+    entryLabel.pack(side=LEFT)
+
+     # Create an Entry Widget in textFrame
+    entryWidget = Entry(textFrame)
+    entryWidget["width"] = 50
+    entryWidget.pack(side=LEFT)
+
+    button = Button(textFrame, text="search", command=searchlist)
+    button.pack(side=LEFT)
+
+    textFrame.pack()
+
+    listbox  = Listbox(main)
+
+    
 
     for row in cursor:
-        print row
+        listbox.insert(END, row)
         
     db.close()
+    listbox.pack(fill=BOTH, expand=1)
+    main.mainloop()
